f8210cdfdf9ebe5b68652786b29eac8d77a7e78b
jonlam1989 <jonlam1989@outlook.com>
2022-04-20 09:24:01 -0400

    added react-icons

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-import { KOREAN_MOVIES, KOREAN_TV_SERIES, KOREAN_TOP_TEN_MOVIES, KOREAN_TOP_TEN_SERIES } from './api-urls';
+import { KOREAN_MOVIES, KOREAN_TV_SERIES, KOREAN_TOP_TEN_MOVIES, KOREAN_TOP_TEN_SERIES } from './api_urls';
 
 const fetchURL = async () => {
 	const response = await fetch(KOREAN_MOVIES);
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,38 @@
 //Styles
 import styles from './Navbar.module.scss';
+//Icons
+import tv_icon from '../../assests/images/tv_icon.svg';
+import { BiMenuAltRight } from 'react-icons/bi';
+import { RiCloseLine } from 'react-icons/ri';
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<nav>
+			<div>
+				<h2>SK-TV</h2>
+				<img src={tv_icon} alt='brand-logo' />
+			</div>
+			<div>
+				<ul>
+					<li>
+						<a href='#'>Movies</a>
+					</li>
+					<li>
+						<a href='#'>Top 10 Movies</a>
+					</li>
+					<li>
+						<a href='#'>TV Shows</a>
+					</li>
+					<li>
+						<a href='#'>Top 10 TV Shows</a>
+					</li>
+				</ul>
+			</div>
+			<div>
+				<BiMenuAltRight />
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
