550518fd8d9b4a74285603f46c5fa7e5fe4975df
jonlam1989 <jonlam1989@outlook.com>
2022-04-21 11:28:02 -0400

    updated fns to fetch data from TMDB api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,27 @@
 import { KOREAN_MOVIES, KOREAN_TV_SERIES, KOREAN_TOP_TEN_MOVIES, KOREAN_TOP_TEN_SERIES } from './api_urls';
 
-const fetchURL = async () => {
+const fetchMovies = async () => {
 	const response = await fetch(KOREAN_MOVIES);
 	const data = await response.json();
 	console.log(data);
 };
+const fetchTopTenMovies = async (id: string) => {
+	const url = KOREAN_TOP_TEN_MOVIES.replace('{id}', id);
+	const response = await fetch(url);
+	const data = await response.json();
+	console.log(data);
+};
+const fetchTvSeries = async () => {
+	const response = await fetch(KOREAN_TV_SERIES);
+	const data = await response.json();
+	console.log(data);
+};
+const fetchTopTenSeries = async (id: string) => {
+	const url = KOREAN_TOP_TEN_SERIES.replace('{id}', id);
+	const response = await fetch(url);
+	const data = await response.json();
+	const results = data.tv_results[0];
+	console.log(results);
+};
 
-export { fetchURL };
+export { fetchMovies, fetchTopTenMovies, fetchTvSeries, fetchTopTenSeries };
