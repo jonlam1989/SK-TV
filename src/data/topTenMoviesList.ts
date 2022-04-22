9db8ed3d23c317906840ae59d40bd649c618426b
jonlam1989 <jonlam1989@outlook.com>
2022-04-22 14:07:01 -0400

    added list for top-ten movies & top-ten tv series

---
+++ src/data/topTenMoviesList.ts
@@ -0,0 +1,12 @@
+export const imdbMovieIds = {
+	War_of_Arrows: 'tt2025526',
+	Train_to_Busan: 'tt5700672',
+	Exit: 'tt10648440',
+	Parasite: 'tt6751668',
+	Ode_to_My_Father: 'tt3812366',
+	A_Taxi_Driver: 'tt6878038',
+	Luck_Key: 'tt6175078',
+	Masquerade: 'tt2290840',
+	The_Pirates: 'tt3485166',
+	The_Pirates_The_Last_Royal_Treasure: 'tt17491040'
+};
