5cd9aebb298a3ba4a5828e4230ea04a40cec91ed
jonlam1989 <jonlam1989@outlook.com>
2022-04-25 10:50:17 -0400

    added Spinner component

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,8 @@
+//Styles
+import styles from './Spinner.module.scss';
+
+const Spinner = () => {
+	return <div className={styles.spinner} />;
+};
+
+export default Spinner;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Navbar } from './Navbar/Navbar';
 export { default as HeroImage } from './HeroImage/HeroImage';
+export { default as Spinner } from './Spinner/Spinner';
