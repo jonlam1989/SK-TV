57dc1ed83ec88f0b4eba8d83b2c5e0d95dbe386b
jonlam1989 <jonlam1989@outlook.com>
2022-04-27 14:35:47 -0400

    added functionality to load default poster onError

--- src/components/GridItem/GridItem.tsx
+++ src/components/GridItem/GridItem.tsx
@@ -2,6 +2,8 @@
 import styles from './GridItem.module.scss';
 //API
 import { IMAGE_BASE_URL } from '../../api/api_urls';
+//PNGs
+import default_poster from '../../assests/images/default_poster.png';
 
 //Types
 type Props = {
@@ -9,7 +11,19 @@ type Props = {
 };
 
 const GridItem: React.FC<Props> = ({ poster_path }) => {
-	return <img className={styles.gridItemImage} src={`${IMAGE_BASE_URL}/${poster_path}`} alt='poster' />;
+	//uses a default poster, if no original poster is found
+	const imgOnErrorHandler = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+		e.currentTarget.src = default_poster;
+	};
+
+	return (
+		<img
+			className={styles.gridItemImage}
+			src={`${IMAGE_BASE_URL}/${poster_path}`}
+			alt='poster'
+			onError={imgOnErrorHandler}
+		/>
+	);
 };
 
 export default GridItem;
