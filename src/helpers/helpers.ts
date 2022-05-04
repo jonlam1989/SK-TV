26d82e31004cf9a7ccafba0e5773ad607b962634
jonlam1989 <jonlam1989@outlook.com>
2022-05-04 11:01:10 -0400

    updated fn to convert time in helpers (v2)

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -2,7 +2,7 @@ const convertTime = (time: number) => {
 	const hours = Math.floor(time / 60);
 	const mins = time % 60;
 
-	return `${hours}${hours > 1 ? 'hrs' : 'hr'} ${mins}mins`;
+	return `${hours}${hours > 1 ? 'hrs' : 'hr'} ${mins}${mins > 1 ? 'mins' : 'min'}`;
 };
 
 export default convertTime;
