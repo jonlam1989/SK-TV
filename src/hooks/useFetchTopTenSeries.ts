1f53532c2c6882e7c121cf5f1187d90cb01ba6ef
jonlam1989 <jonlam1989@outlook.com>
2022-05-13 10:32:51 -0400

    added useQueries to useFetchTopTenSeries

--- src/hooks/useFetchTopTenSeries.ts
+++ src/hooks/useFetchTopTenSeries.ts
@@ -1,46 +1,19 @@
-import { useState, useEffect } from 'react';
+//Query
+import { useQueries } from 'react-query';
 //API
 import { fetchTopTenSeries } from '../api/api';
-import { SpecificTvSeriesType } from '../api/api_types';
 //data
 import { imdbSeriesIds } from '../data/topTenSeriesList';
 
-const initialState = [] as SpecificTvSeriesType[];
-
 const useFetchTopTenSeries = () => {
-	const [ state, setState ] = useState(initialState);
-	const [ loading, setLoading ] = useState(false);
-	const [ error, setError ] = useState(false);
-
-	//either store data from api in state or throw an error
-	const getData = async () => {
-		try {
-			setLoading(true);
-			setError(false);
-
-			//use a temporary-data array to prevent multiple re-renders
-			//if had used setState((prev)=>[...prev, specificTvSeries]):
-			//    -there would have been a re-render for each iteration in the for-loop
-			let tempData = [];
-			for (let imdbID of Object.values(imdbSeriesIds)) {
-				const specificTvSeries = await fetchTopTenSeries(imdbID);
-				tempData.push(specificTvSeries);
-			}
-			setState([ ...tempData ]);
-
-			setLoading(false);
-		} catch (error) {
-			setError(true);
-			setLoading(false);
-		}
-	};
-
-	//get data once the page loads
-	useEffect(() => {
-		getData();
-	}, []);
-
-	return { state, loading, error };
+	return useQueries(
+		Object.values(imdbSeriesIds).map((id) => {
+			return {
+				queryKey: [ 'top-ten-series', id ],
+				queryFn: () => fetchTopTenSeries(id)
+			};
+		})
+	);
 };
 
 export default useFetchTopTenSeries;
--- src/pages/TopTenTvSeries.tsx
+++ src/pages/TopTenTvSeries.tsx
@@ -1,35 +1,40 @@
 //Components
 import { Grid, GridItem, HeroImage, Spinner } from '../components';
+//API
+import { SpecificTvSeriesType } from '../api/api_types';
 //custom hook
 import useFetchTopTenSeries from '../hooks/useFetchTopTenSeries';
 
 const TopTenTvSeries: React.FC = () => {
-	const { state, loading, error } = useFetchTopTenSeries();
-	console.log(state);
+	const data = useFetchTopTenSeries();
+	console.log(data);
+	//check if the first query has finished loading successfully
+	//the HeroImage will take about 3s to animate
+	//which should give the rest of the queries some time to complete behind the scenes
+	const { error, isLoading } = data[0];
+	//check if all the queries have finished loading successfully
+	const allSuccessful = data.every((query) => query.status === 'success');
 
 	if (error) return <div>Something went wrong...</div>;
 
 	return (
 		<main>
-			{loading && <Spinner />}
-			{state[0] && (
-				<>
-					<HeroImage 
-						backdrop_path={state[0].backdrop_path} 
-						title={state[0].name} 
-						overview={state[0].overview} 
-					/>
-					<Grid currPage='Top 10 TV Series'>
-						{state.map((tv) => 
-							<GridItem 
-								key={tv.id} 
-								id={tv.id} 
-								poster_path={tv.poster_path} 
-								type='tv'
-							/>
-						)}
-					</Grid>
-				</>
+			{isLoading && <Spinner />}
+			{!isLoading && (
+				<HeroImage
+					backdrop_path={data[0].data.backdrop_path}
+					title={data[0].data.name}
+					overview={data[0].data.overview}
+				/>
+			)}
+			{allSuccessful && (
+				<Grid currPage='Top 10 TV Series'>
+					{data.map((query) => {
+						const tv: SpecificTvSeriesType = query.data;
+
+						return <GridItem key={tv.id} id={tv.id} poster_path={tv.poster_path} type='tv' />;
+					})}
+				</Grid>
 			)}
 		</main>
 	);
